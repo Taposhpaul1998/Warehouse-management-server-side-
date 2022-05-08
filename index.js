5342218dbea6510f224d60d19573936d624b9c82
Taposh paul <taposhp05@gamil.com>
2022-05-08 19:32:28 +0600

    cread delete method and post method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,29 +11,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9swh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('Cyclehouse').collection('products');
-
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productsCollection.findOne(query);
+            res.send(products);
+        });
+        //POST 
+        app.post('/products', async (req, res) => {
+            const newProducts = req.body;
+            const result = await productsCollection.insertOne(newProducts);
+            res.send(result)
+        })
+        // delete
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
 
     }
     finally {
 
     }
-
 };
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Ranning Cycle House server');
