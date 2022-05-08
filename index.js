9916439ba26579cbccc32bcb6f118a31dafffb3b
Taposh paul <taposhp05@gamil.com>
2022-05-08 23:18:30 +0600

    diploy

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9swh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,6 +19,26 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('Cyclehouse').collection('products');
+        const pricingCollection = client.db('Cyclehouse').collection('pricing');
+        const categoryCollection = client.db('Cyclehouse').collection('category');
+
+        // category 
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const category = await cursor.toArray();
+            res.send(category);
+        });
+
+        // pricing 
+        app.get('/pricing', async (req, res) => {
+            const query = {};
+            const cursor = pricingCollection.find(query);
+            const pricing = await cursor.toArray();
+            res.send(products);
+        });
+
+        // server api 
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
